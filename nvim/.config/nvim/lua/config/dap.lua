b0cacd48196e82f6f983a6395f5227dc45db7296
joytsay <joytsay@hotmail.com>
2026-02-06 09:40:18 +0800

    Wrap python table exists and only runs after dap loads

--- nvim/.config/nvim/lua/config/dap.lua
+++ nvim/.config/nvim/lua/config/dap.lua
@@ -1,26 +1,29 @@
--- require("dap-python").setup("/nfs/home/joy.tsay/miniconda3/bin/python")
--- print("Before")
--- print(vim.inspect(require("dap").configurations.python))
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
 
-table.insert(require("dap").configurations.python, {
-  type = "python",
-  request = "attach",
-  name = "Python Remote Container Attach",
-  host = "localhost",
-  port = 5678,
-  mode = "remote",
-  cwd = vim.fn.getcwd(),
-  pathMappings = {
-    {
-      localRoot = function()
-        return vim.fn.input("Local code folder > ", vim.fn.getcwd(), "file")
-      end,
-      remoteRoot = function()
-        return vim.fn.input("Container code folder > ", ".", "file")
-      end,
-    },
-  },
-})
+    -- Ensure the python config table exists
+    dap.configurations.python = dap.configurations.python or {}
 
--- print("After")
--- print(vim.inspect(require("dap").configurations.python))
+    table.insert(dap.configurations.python, {
+      type = "python",
+      request = "attach",
+      name = "Python Remote Container Attach",
+      host = "localhost",
+      port = 5678,
+      mode = "remote",
+      cwd = vim.fn.getcwd(),
+      pathMappings = {
+        {
+          localRoot = function()
+            return vim.fn.input("Local code folder > ", vim.fn.getcwd(), "file")
+          end,
+          remoteRoot = function()
+            return vim.fn.input("Container code folder > ", ".", "file")
+          end,
+        },
+      },
+    })
+  end,
+}
